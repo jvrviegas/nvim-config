dd22991640763c304ddd06679229001f7f673264
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2022-10-17 14:37:46 -0300

    feat: add blamer configs

--- lua/jvrviegas/plugins/init.lua
+++ lua/jvrviegas/plugins/init.lua
@@ -12,6 +12,4 @@ require("jvrviegas.plugins.trouble")
 require("jvrviegas.plugins.toggleterm")
 require("jvrviegas.plugins.lsp_kind")
 require("jvrviegas.plugins.lspsaga")
-
-vim.cmd([[let g:blamer_enabled = 1]])
-vim.cmd([[let g:blamer_delay = 500]])
+require("jvrviegas.plugins.blamer")
