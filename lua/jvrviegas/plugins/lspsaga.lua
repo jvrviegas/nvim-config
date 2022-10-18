551dc537e18a6d2ea4d2376bade8cf7bd2f8832d
João Vitor Viegas <joao.viegas@rummo.com>
2022-10-18 08:10:04 -0300

    refactor(lspsaga): fix shortcut

--- lua/jvrviegas/plugins/lspsaga.lua
+++ lua/jvrviegas/plugins/lspsaga.lua
@@ -1,16 +1,18 @@
-local status, saga = pcall(require, "lspsaga")
-if (not status) then return end
+local status, saga = pcall(require, 'lspsaga')
+if not status then
+  return
+end
 
 local keymap = vim.keymap.set
 
-saga.init_lsp_saga {
+saga.init_lsp_saga({
   server_filetype_map = {
-    typescript = 'typescript'
+    typescript = 'typescript',
   },
   code_action_lightbulb = {
     enable = false,
   },
-}
+})
 
 local opts = { noremap = true, silent = true }
 keymap('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
@@ -18,7 +20,7 @@ keymap('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 keymap('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 keymap('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 keymap('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
-keymap("n", "<leader>a", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-keymap("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+keymap('n', '<leader>a', '<cmd>Lspsaga show_line_diagnostics<CR>', opts)
+keymap('n', '<leader>d', '<cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
+keymap('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+keymap('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
