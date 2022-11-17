39bf55f89bddf6b7574fb2374fcb30672beb9478
João Vitor Viegas <joao.viegas@rummo.com>
2022-11-17 10:57:58 -0300

    refactor(sumneko_lua): update neodev config

--- lua/jvrviegas/plugins/lsp/sumneko_lua.lua
+++ lua/jvrviegas/plugins/lsp/sumneko_lua.lua
@@ -2,17 +2,12 @@
 
 local settings = {
   Lua = {
-    diagnostics = {
-      globals = {
-        "vim",
-      },
-    },
     completion = {
-      callSnippet = "Replace",
+      callSnippet = 'Replace',
     },
     workspace = {
       checkThirdParty = false,
-      library = vim.api.nvim_get_runtime_file("", true),
+      -- library = vim.api.nvim_get_runtime_file('', true),
     },
   },
 }
@@ -20,14 +15,12 @@ local settings = {
 local M = {}
 
 M.setup = function(on_attach, capabilities)
-  local neodev = require("neodev").setup({
-    lspconfig = {
-      on_attach = on_attach,
-      settings = settings,
-      capabilities = capabilities,
-    },
+  require('neodev').setup({})
+  require('lspconfig')['sumneko_lua'].setup({
+    on_attach = on_attach,
+    settings = settings,
+    capabilities = capabilities,
   })
-  require("lspconfig")['sumneko_lua'].setup(neodev)
 end
 
 return M
