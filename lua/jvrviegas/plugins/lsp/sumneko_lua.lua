c30639b7d9e3b7127f7a952f9bc3481ed857e8a5
João Vitor Viegas <joao.viegas@rummo.com>
2022-11-17 12:16:34 -0300

    refactor(sumneko_lua): disable sumneko_lua formatting to prevent conflicts with stylua

--- lua/jvrviegas/plugins/lsp/sumneko_lua.lua
+++ lua/jvrviegas/plugins/lsp/sumneko_lua.lua
@@ -17,7 +17,12 @@ local M = {}
 M.setup = function(on_attach, capabilities)
 	require('neodev').setup({})
 	require('lspconfig')['sumneko_lua'].setup({
-		on_attach = on_attach,
+		on_attach = function(client, bufnr)
+			client.server_capabilities.documentFormattingProvider = false
+			client.server_capabilities.documentRangeFormattingProvider = false
+
+			on_attach(client, bufnr)
+		end,
 		settings = settings,
 		capabilities = capabilities,
 	})
