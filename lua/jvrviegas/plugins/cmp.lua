79d3ab0112f6fcd01051d1420d998b7867c42207
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2023-01-23 20:28:28 -0300

    refactor(cmp): add luasnip

--- lua/jvrviegas/plugins/cmp.lua
+++ lua/jvrviegas/plugins/cmp.lua
@@ -14,17 +14,19 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		['<C-d>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-s>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+		['<C-s>'] = cmp.mapping(cmp.mapping.complete({}), { 'i', 'c' }),
 		['<C-e>'] = cmp.mapping.close(),
 		['<CR>'] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
 	}),
-	sources = {
+	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' }, -- For luasnip users.
+	}, {
 		{ name = 'buffer' },
-	},
+	}),
 	formatting = {
 		format = lspkind.cmp_format({
 			maxwidth = 50,
