548cf82442b4368e962998d8c46f7b21e97f9d6b
João Vitor Viegas <joao.viegas@rummo.com>
2023-02-27 17:00:40 -0300

    refactor(nvim-tree): update open on start

--- lua/jvrviegas/plugins/nvim-tree.lua
+++ lua/jvrviegas/plugins/nvim-tree.lua
@@ -1,7 +1,6 @@
 local nvim_tree = require('nvim-tree')
 
 nvim_tree.setup({
-	open_on_setup = true,
 	view = {
 		side = 'left',
 		width = 30,
@@ -12,3 +11,20 @@ nvim_tree.setup({
 		},
 	},
 })
+
+local function open_nvim_tree(data)
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not directory then
+		return
+	end
+
+	-- change to the directory
+	vim.cmd.cd(data.file)
+
+	-- open the tree
+	require('nvim-tree.api').tree.open()
+end
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
