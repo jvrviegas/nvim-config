7cbd7a0af8af8458d8ab5e41be0435f818b2820d
João Viegas <joao.viegas@rummo.com>
2023-06-20 19:39:28 -0300

    chore: replaced old config with kickstart.nvim

--- lua/jvrviegas/plugins/blamer.lua
+++
@@ -1,2 +0,0 @@
-vim.g.blamer_enabled = 0
-vim.g.blamer_show_in_inset_modes = 0
--- lua/jvrviegas/plugins/trouble.lua
+++
@@ -1,8 +0,0 @@
-local trouble = require('trouble')
-
-trouble.setup({
-	action_keys = {
-		jump = { 'o', 'tab' },
-		jump_close = '<cr>',
-	},
-})
