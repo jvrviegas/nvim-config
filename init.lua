d3613ae0e5d703d0467a2bbd8ead52dfbb9a9f80
João Viegas <joao.viegas@rummo.com>
2023-06-20 21:13:03 -0300

    feat(plugins): change theme to catppuccin

--- init.lua
+++ init.lua
@@ -146,10 +146,10 @@ require('lazy').setup({
 
 	{
 		-- Theme inspired by Atom
-		'navarasu/onedark.nvim',
+		'catppuccin/nvim',
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme('onedark')
+			vim.cmd.colorscheme('catppuccin')
 		end,
 	},
 
