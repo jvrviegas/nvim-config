46ae68c347367bb25f544caac3ac3f707e7d4149
João Viegas <joao.viegas@rummo.com>
2023-07-13 16:53:08 -0300

    refactor(settings): add custom settings

---
+++ lua/custom/settings.lua
@@ -0,0 +1,12 @@
+-- Set relative numbers
+vim.o.relativenumber = true
+
+-- Set custom diagnostic signs
+vim.fn.sign_define("DiagnosticSignError",
+	{ text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn",
+	{ text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo",
+	{ text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint",
+	{ text = "", texthl = "DiagnosticSignHint" })
