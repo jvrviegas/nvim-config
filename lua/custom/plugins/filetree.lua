d74274ebfb4df3faed5154304af8ea6b42d7aa7d
João Viegas <joao.viegas@rummo.com>
2023-10-24 16:57:47 -0300

    refactor(filetree): add event to close filetree when a file is opened

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -19,6 +19,17 @@ return {
           hide_dotfiles = false,
         },
       },
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            -- auto close
+            -- vimc.cmd("Neotree close")
+            -- OR
+            require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+      }
     })
   end,
 }
