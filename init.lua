af006602e33f877130190aa153cb00a1d51a93b2
João Viegas <joao.viegas@rummo.com>
2023-11-09 22:25:56 -0300

    chore(init): set catppuccin background to transparent

--- init.lua
+++ init.lua
@@ -149,6 +149,9 @@ require('lazy').setup({
     'catppuccin/nvim',
     priority = 1000,
     config = function()
+      require('catppuccin').setup({
+        transparent_background = true
+      })
       vim.cmd.colorscheme('catppuccin')
     end,
   },
