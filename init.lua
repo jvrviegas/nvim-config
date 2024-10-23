8f709a987cbb19617ab1e25a0cdea5c757740c8c
João Viegas <joao.viegas@rummo.com>
2024-10-23 08:38:26 -0300

    refactor(init.lua): add new languages to treesitter required install

--- init.lua
+++ init.lua
@@ -349,7 +349,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'lua', 'rust', 'javascript', 'tsx', 'typescript', 'vimdoc', 'vim', 'prisma', 'sql' },
+    ensure_installed = { 'lua', 'rust', 'javascript', 'tsx', 'typescript', 'vimdoc', 'vim', 'prisma', 'sql', 'http', 'go' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
