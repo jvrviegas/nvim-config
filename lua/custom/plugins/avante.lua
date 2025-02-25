b6e467559a044b38d81287a1b64d20cdd23b4412
João Viegas <joao.viegas@rummo.com>
2025-02-24 21:37:53 -0300

    refactor(avante.nvim): update claude model

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -5,6 +5,11 @@ return {
   version = false, -- set this if you want to always pull the latest change
   opts = {
     -- add any opts here
+    provider = "claude",
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-3-7-sonnet-20250219"
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
