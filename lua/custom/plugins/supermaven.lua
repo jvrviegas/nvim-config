20fd7aeb91cdd169a0ee8faa713427cbea7130ae
João Viegas <joao.viegas@rummo.com>
2025-03-06 20:44:45 -0300

    refactor(ai): switch supermaven with codeium

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       require("supermaven-nvim").setup({
         keymaps = {
-          accept_suggestion = "<Tab>",
+          accept_suggestion = "<S-Tab>",
           clear_suggestion = "<C-c>",
           accept_word = "<C-j>",
         },
@@ -17,7 +17,7 @@ return {
         disable_inline_completion = false, -- disables inline completion for use with cmp
         disable_keymaps = false,           -- disables built in keymaps for more manual control
         condition = function()
-          return false
+          return true
         end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
       })
     end,
