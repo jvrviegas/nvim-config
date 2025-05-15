8149818015e98477da97959d454f81fb7e331063
João Viegas <joao.viegas@rummo.com>
2025-05-15 15:15:34 -0300

    refactor(init): update mason lspconfig initialization

--- init.lua
+++ init.lua
@@ -445,17 +445,8 @@ local capabilities = require('blink.cmp').get_lsp_capabilities()
 local mason_lspconfig = require('mason-lspconfig')
 
 mason_lspconfig.setup({
+  automatic_enable = true,
   ensure_installed = vim.tbl_keys(servers),
-})
-
-mason_lspconfig.setup_handlers({
-  function(server_name)
-    require('lspconfig')[server_name].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    })
-  end,
 })
 
 require('lspconfig').eslint.setup({
