62c45a739f11f585df8bdd0763d22e090d5d5a1c
João Viegas <joao.viegas@rummo.com>
2025-07-14 22:21:48 +0100

    refactor(filetree): update configs

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,35 +1,34 @@
--- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
-  config = function()
-    vim.keymap.set("n", "<leader>nt", ":Neotree toggle<cr>", { desc = "[N]eotree [T]oggle" })
-    vim.keymap.set("n", "<leader>rf", ":Neotree reveal<cr>", { desc = "Neotree [R]eveal [F]ile" })
-
-    require("neo-tree").setup({
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
         },
       },
-      event_handlers = {
-        {
-          event = "file_opened",
-          handler = function(file_path)
-            -- auto close
-            -- vimc.cmd("Neotree close")
-            -- OR
-            require("neo-tree.command").execute({ action = "close" })
-          end
-        },
-      }
-    })
-  end,
+    },
+    event_handlers = {
+      {
+        event = 'file_open_requested',
+        handler = function()
+          -- auto close
+          -- vim.cmd("Neotree close")
+          -- OR
+          require('neo-tree.command').execute { action = 'close' }
+        end,
+      },
+    },
+  },
 }
+
